d29c79e14b79b051e38577d153a033ca8b72d221
MrValdez <shunyvaldez@gmail.com>
2015-04-07 19:50:36 +0800

    added action

--- main.py
+++ main.py
@@ -7,3 +7,14 @@
 #  5. If challenge fails, kill challenger's one card
 #  5.a. Check if player is dead
 #  6. Play card's effect if no challenge or challenge fails
+
+class Action:
+    name = "Income"
+    description = "Gain 1 gold"
+    
+    def __init__(self):
+        pass
+        
+    def play(self, player, target = None):
+        # should be overrriden by child classes
+        pass

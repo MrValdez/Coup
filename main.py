f13c83d76fae3653b54f33bd4171e1181f8b2f35
MrValdez <shunyvaldez@gmail.com>
2015-04-07 19:48:37 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# play order:
+#  1. Choose an action to play or bluff
+#  2. Announce to everyone the play
+#  3. Allow other players to challenge move
+#  4. If challenge succeeds, kill the player's one card
+#  4.a. Check if player is dead
+#  5. If challenge fails, kill challenger's one card
+#  5.a. Check if player is dead
+#  6. Play card's effect if no challenge or challenge fails

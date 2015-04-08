378b69a5f7d3a7168ab83c357b1ea09aca140fda
MrValdez <shunyvaldez@gmail.com>
2015-04-08 12:05:31 +0800

    updated game flow

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # play order:
 #  1. Choose an action to play or bluff
 #  2. Announce to everyone the play
-#  3. Allow other players to challenge move
-#  4. If challenge succeeds, kill the player's one card
-#  4.a. Check if player is dead
-#  5. If challenge fails, kill challenger's one card
-#  5.a. Check if player is dead
-#  6. Play card's effect if no challenge or challenge fails
+#  3. Allow other players to block move
+#  3.a. Allow active player to call bluff
+#  3.b. Challenge Check
+#  4. Allow other players to challenge move
+#  5. Play card's effect if no challenge or blocking fails
+
+#  Challenge Check: 
+#    1. If challenge succeeds, kill the player's one card
+#    2. Check if player is dead
+#    3. If challenge fails, kill challenger's one card
+#    4. Check if player is dead

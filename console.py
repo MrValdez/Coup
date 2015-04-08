78cb5d0368d00a584690cf2573d376d2f7d98570
MrValdez <shunyvaldez@gmail.com>
2015-04-08 18:54:23 +0800

    refactored to functions

--- console.py
+++ console.py
@@ -4,40 +4,52 @@ from game   import GameState
 
 import random
 
-# How many people are playing?
-# Generate the player list
-# Shuffle the player list
-# Infinite loop until one player remains
-
-PlayerCount = int(input("How many players? "))
-#PlayerCount = 2
-
 Players = []
-def CreatePlayer(Number):
-    player = Player()
-    
-    print("Player #%i" % (Number))
-    player.name = input(" What is your name? ")
-    #player.name = random.choice(["A", "B", "C"]) + str(Number)
-    
-    return player
-
-for i in range(PlayerCount):
-    Players.append(CreatePlayer(i))
-    
-random.shuffle(Players)
-
-PlayersAlive = [player for player in Players if player.alive]
+PlayersAlive = []
 CurrentPlayer = 0
 
-print ("Turn order:")
-for i, player in enumerate(Players):
-    print(" %i: %s" % (i + 1, player.name))
+def Setup():
+    # How many people are playing?
+    # Generate the player list
+    # Shuffle the player list
 
-while len(PlayersAlive) > 1:
-    print("Action for %s: " % (Players[CurrentPlayer].name))
-    
-    CurrentPlayer += 1
-    if CurrentPlayer >= len(Players): CurrentPlayer = 0
+    #PlayerCount = int(input("How many players? "))
+    PlayerCount = 2
+
+    def CreatePlayer(Number):
+        player = Player()
+        
+        print("Player #%i" % (Number))
+        #player.name = input(" What is your name? ")
+        player.name = random.choice(["A", "B", "C"]) + str(Number)
+        
+        return player
+
+    for i in range(PlayerCount):
+        Players.append(CreatePlayer(i))
+        
+    random.shuffle(Players)
+
+    global PlayersAlive
     PlayersAlive = [player for player in Players if player.alive]
-    input()
+
+def PrintTurnOrder():
+    print ("Turn order:")
+    for i, player in enumerate(Players):
+        print(" %i: %s" % (i + 1, player.name))
+
+def MainLoop():
+    # Infinite loop until one player remains
+    global PlayersAlive, CurrentPlayer
+    
+    while len(PlayersAlive) > 1:
+        print("Action for %s: " % (Players[CurrentPlayer].name))
+        
+        CurrentPlayer += 1
+        if CurrentPlayer >= len(Players): CurrentPlayer = 0
+        PlayersAlive = [player for player in Players if player.alive]
+        input()
+
+Setup()
+PrintTurnOrder()
+MainLoop()
